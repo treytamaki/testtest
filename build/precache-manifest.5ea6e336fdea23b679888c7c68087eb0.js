cbaf9a699557692adce3bcc2dffbec699ec671a8
Trey Tamaki <tgtamaki@uw.edu>
2020-02-12 23:04:52 -0800

    add react

---
+++ build/precache-manifest.5ea6e336fdea23b679888c7c68087eb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6544cf8800c1d68f92e728df96a50357",
+    "url": "/A3-seattle-crime/index.html"
+  },
+  {
+    "revision": "c2cad6a7fa23c5539a3d",
+    "url": "/A3-seattle-crime/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a1fe7500f309fcee1fb8",
+    "url": "/A3-seattle-crime/static/js/2.10923b9e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/A3-seattle-crime/static/js/2.10923b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2cad6a7fa23c5539a3d",
+    "url": "/A3-seattle-crime/static/js/main.3b39babe.chunk.js"
+  },
+  {
+    "revision": "80d8ece77cf339f76143",
+    "url": "/A3-seattle-crime/static/js/runtime-main.c61cc853.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/A3-seattle-crime/static/media/logo.5d5d9eef.svg"
+  }
+]);
