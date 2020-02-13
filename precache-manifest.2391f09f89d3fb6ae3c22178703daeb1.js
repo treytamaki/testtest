70da2e558e471bd039961bcdf854cb4850aaa861
Trey Tamaki <tgtamaki@uw.edu>
2020-02-12 23:07:06 -0800

    Updates

---
+++ precache-manifest.2391f09f89d3fb6ae3c22178703daeb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbef995fc1f71ba60b58592b1f1ea89b",
+    "url": "/testtest/index.html"
+  },
+  {
+    "revision": "b714b68b76881858083e",
+    "url": "/testtest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d94a2a0c75b759c31eb8",
+    "url": "/testtest/static/js/2.97d07914.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/testtest/static/js/2.97d07914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b714b68b76881858083e",
+    "url": "/testtest/static/js/main.21e17028.chunk.js"
+  },
+  {
+    "revision": "64c84a27da219e40ee4f",
+    "url": "/testtest/static/js/runtime-main.7d88fb51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testtest/static/media/logo.5d5d9eef.svg"
+  }
+]);
